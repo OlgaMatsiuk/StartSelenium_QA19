171dbe5dc948a15b7b754dcc7a4033178f23d1c0
OlgaMatsiuk <matsiuk2015@gmail.com>
2024-06-14 21:31:54 +0300

    cyberm8

--- src/test/java/Start_Test.java
+++ src/test/java/Start_Test.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -17,73 +18,34 @@ public class Start_Test {
     @BeforeTest
     public void precondition(){
         wd=new ChromeDriver();
-        wd.get("https://telranedu.web.app/home");
+        wd.get("https://testing.cloud.cyberm8.com/");
+        wd.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 
 
     }
+    public void type(By locator, String text){
+        WebElement element = wd.findElement(locator);
+        element.click();
+        element.clear();
+        element.sendKeys(text);
+    }
+    public void click (By locator){
+        wd.findElement(locator).click();
+    }
+
 
     @Test
     public void testMethod(){
-        wd.navigate().to("https://www.leumi.co.il/");
-        wd.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-        WebElement element=wd.findElement(By.xpath("//nav[@class='menuCategory']/ul/li[1]//span"));
-        //System.out.println(element.getText());
-       String text =  element.getText();
-
-        System.out.println(text.charAt(0));
-        //var button = document.querySelector("button");
-       // var buttonText = element.inn
-       //console.log(buttonText);
-
-
-
-//        wd.findElement(By.tagName("a"));
-//        wd.findElement(By.cssSelector("a"));
-//        wd.findElement(By.xpath("//a"));
-//
-//
-//
-//        wd.findElement(By.id("root"));
-//        wd.findElement(By.cssSelector("#root"));
-//        wd.findElement(By.xpath("//*[@id='root']"));
-//
-//        wd.findElement(By.linkText("HOME"));
-//        wd.findElement(By.xpath("//*[text()='HOME']"));
-//        wd.findElement(By.xpath("//*[.='HOME']"));
-//
-//
-//
-//
-//
-//        wd.findElement(By.partialLinkText("HOM"));
-//
-//
-//
-//        WebElement element = wd.findElement(By.id("root"));
-//        List<WebElement> list = wd.findElements(By.tagName("div"));
-//        System.out.println(list.size());
-//
-//        wd.findElement(By.cssSelector("[href='/home']"));
-//        wd.findElement(By.xpath("//*[href='/home']"));
-//
-//
-//        wd.findElement(By.cssSelector("[href^='/ho']"));
-//        wd.findElement(By.xpath("//*[start-with(href,'/ho')]"));
-//
-//
-//        wd.findElement(By.cssSelector("[href$='me']")); //no xPath
-//
-//
-//        wd.findElement(By.cssSelector("[href*='om']"));
-//        wd.findElement(By.xpath("//*[contains(@href, 'om'])"));
-
+        type(By.id("username"), "admin");
+        type(By.id("password"), "REZ1ucp2drw8gdp@efj");
+        click(By.id("loginBtn"));
 
     }
 
     @AfterTest
     public void postcondition(){
 
-        wd.quit();
+        //wd.quit();
 
     }
 
